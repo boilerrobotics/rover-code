5b9e3354e962413ca80fbee7490584ceed0a47ff
tame001 <thirawat.tam@gmail.com>
2019-11-15 18:30:02 -0500

    add setup.bash

---
+++ commu-intell/ros/setup.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+source devel/setup.bash
+chmod +x src/beginner_tutorials/srv/AddTwoInts.srv
+chmod +x src/beginner_tutorials/scripts/add_two_ints_client.py src/beginner_tutorials/scripts/add_two_ints_server.py src/beginner_tutorials/scripts/listener.py src/beginner_tutorials/scripts/talker.py
