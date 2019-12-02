8c2406a368e2e115b1e2ede6401a1fc5e5fba39f
tame001 <thirawat.tam@gmail.com>
2019-12-02 18:30:23 -0500

    constuct class

--- commu-intell/ros/src/motor/src/scripts/interface.py
+++ commu-intell/ros/src/motor/src/scripts/interface.py
@@ -4,15 +4,30 @@ import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
-def callback(data):
-    rospy.loginfo(data.linear.x)
+class motor: 
+    def __init__(self):
+        self.speed = 0.5
+        rospy.init_node('motor_listener', anonymous=True)
+        rospy.Subscriber('/cmd_vel', Twist, self.callback)
 
-def listener():
-    rospy.init_node('motor_listener', anonymous=True)
+    def callback(self, data):
+        rospy.loginfo(data.linear.x)
+        self.speed = self.pre_process_speed(data.linear.x)
+        # print(self.speed)
 
-    rospy.Subscriber('/cmd_vel', Twist, callback)
+    def pre_process_speed(self, speed):
+        if abs(speed) > 1:
+            speed = speed/abs(speed)
+        return round(speed, 2)
 
-    rospy.spin()
+    def drive_motor(self):
+        print(self.speed)
 
 if __name__ == '__main__':
-    listener()
+    m = motor() 
+    rospy.spin()
+    try:
+        while True:
+            m.drive_motor()
+    except KeyboardInterrupt:
+        exit(0)
