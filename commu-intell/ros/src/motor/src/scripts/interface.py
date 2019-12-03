8614c5e0ac8c6260b13b7809a15d21152a6ac926
tame001 <thirawat.tam@gmail.com>
2019-12-02 19:40:42 -0500

    add pwm

--- commu-intell/ros/src/motor/src/scripts/interface.py
+++ commu-intell/ros/src/motor/src/scripts/interface.py
@@ -3,10 +3,12 @@
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
+from gpiozero import Motor
 
-class motor: 
+class motor_driver: 
     def __init__(self):
         self.speed = 0.5
+        self.motor = Motor(forward=4, backward=14)
         rospy.init_node('motor_listener', anonymous=True)
         rospy.Subscriber('/cmd_vel', Twist, self.callback)
 
@@ -21,11 +23,18 @@ class motor:
         return round(speed, 2)
 
     def drive_motor(self):
-        print(self.speed)
+        # print(self.speed)
+        if self.speed > 0:
+            self.motor.forward(self.speed)
+        elif self.speed < 0:
+            self.motor.backward(self.speed)
+        else:
+            self.motor.stop()
+
 
 if __name__ == '__main__':
-    m = motor() 
-    rospy.spin()
+    m = motor_driver() 
+    # rospy.spin()
     try:
         while True:
             m.drive_motor()
