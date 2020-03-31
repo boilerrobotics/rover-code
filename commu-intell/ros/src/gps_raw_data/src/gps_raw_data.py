9807244eb06edb528ed1d630815219028c58d45a
Madhuri Vempati <sirimadhuri.v@gmail.com>
2020-03-31 19:00:07 -0400

    code for raw data

---
+++ commu-intell/ros/src/gps_raw_data/src/gps_raw_data.py
@@ -0,0 +1,17 @@
+import serial
+import time
+import string
+import pynmea2
+
+while True:
+	port="/dev/ttyAMA0"
+	ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+	dataout = pynmea2.NMEAStreamReader()
+	newdata=ser.readline()
+
+	if newdata[0:6] == "$GPRMC":
+		newmsg=pynmea2.parse(newdata)
+		lat=newmsg.latitude
+		lng=newmsg.longitude
+		gps = "Latitude = " + str(lat) + "and Longitude = " + str(lng)
+		print(gps)
