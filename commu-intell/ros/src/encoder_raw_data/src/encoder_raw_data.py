a042e72d51a63b2ea9aa4aa668e29d7ab350d512
pumkhai <pkhai@purdue.edu>
2020-04-02 00:24:39 -0400

    today edited April Fool Day

---
+++ commu-intell/ros/src/encoder_raw_data/src/encoder_raw_data.py
@@ -0,0 +1,16 @@
+#That number should be the 
+#rotations per second of the wheel. 
+#You don't need the wheel diameter anymore. 
+#So you will start of with some raw data, 
+#you will then get the interval from that with some 
+#processing and use that 
+#to calculate rotations per second.
+# You will then publish that number.
+
+## Find a way to import some signal from the encoder
+
+
+## Some how find a way to Calculate rotation per seconds
+
+
+# Publish that number in float format.
