6f2ce34ecea84e0196b13a386d4768f0887245df
pkhai@purdue.edu <pkhai@purdue.edu>
2020-04-04 19:23:00 -0400

    19:22

--- commu-intell/ros/src/encoder_raw_data/src/encoder_raw_data.py
+++ commu-intell/ros/src/encoder_raw_data/src/encoder_raw_data.py
@@ -1,13 +1,3 @@
-
-#That number should be the 
-#rotations per second of the wheel. 
-#You don't need the wheel diameter anymore. 
-#So you will start of with some raw data, 
-#you will then get the interval from that with some 
-#processing and use that 
-#to calculate rotations per second.
-# You will then publish that number.
-
 ## Find a way to import some signal from the encoder
 import math as math
 import rospy
@@ -26,32 +16,7 @@ def calculate_angular_velocity(pulses_per_second):
     return (pulses_per_second / pulses_per_rotations * 2 * math.pi)
 
 def calculate_linear_velocity(pulses_per_second):
-   #Distance traveled = (Degrees turned / 360) * circumference
    diameter = 0.2
-   #Circumference = math.Pi * diameter
-   #Degree = 5000 # some random number to initialize
-   #degree_per_min = Degree
-   #time_in_min = 60
-   #Distance_traveled = (Degree / 360) * Circumference 
-   #Speed = Distance_traveled * time_in_min
-   # Angular Speed ω = 2πn/Nt
-   # ω = angular speed (rad/s)
-   # n = number of pulses
-   # t = sampling period (s)
-   # N = pulses per rotation
-   # fasfsg
-   # fasfaf
-   # Linear 
-   # We want linear 
-   #n = read_num_Of_pulses()
-   #t = read_time()
-   #N = read_pulse_per_rotation
-   #ω = 2 * math.pi * n / N * t
-
-    ## vel = angular * radius 
-    # Use 360 
-    # 
-
    return calculate_angular_velocity(pulses_per_second) * diameter / 2
 
 ## Publish that number in float format.
