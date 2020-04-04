c74d9d10ebf65d332e5c8315a67e214bdef2892e
pkhai@purdue.edu <pkhai@purdue.edu>
2020-04-04 19:18:32 -0400

    new changes

--- commu-intell/ros/src/encoder_raw_data/src/encoder_raw_data.py
+++ commu-intell/ros/src/encoder_raw_data/src/encoder_raw_data.py
@@ -13,46 +13,57 @@ import math as math
 import rospy
 from std_msgs.msg._String import String
 
-def read_num_Of_pulses():
+# keeping counting pulses
+# def getEncoder once get the hardware
+
+def get_num_Of_pulses(period):
+    # return number of pulses in that period
    return 10
 
-def read_time():
-   return 60
-
-def read_pulse_per_rotation():
-   return (read_num_Of_pulses / read_time)
-
 ## Some how find a way to Calculate rotation per seconds
-def calculateSpeed():
+def calculate_angular_velocity(pulses_per_second):
+    pulses_per_rotations = 30 # go to encoder data sheet
+    return (pulses_per_second / pulses_per_rotations * 2 * math.pi)
+
+def calculate_linear_velocity(pulses_per_second):
    #Distance traveled = (Degrees turned / 360) * circumference
-   Diameter = 0.2
-   Circumference = math.Pi * Diameter
-   Degree = 5000 # some random number to initialize
-   degree_per_min = Degree
-   time_in_min = 60
-   Distance_traveled = (Degree / 360) * Circumference 
-   Speed = Distance_traveled * time_in_min
+   diameter = 0.2
+   #Circumference = math.Pi * diameter
+   #Degree = 5000 # some random number to initialize
+   #degree_per_min = Degree
+   #time_in_min = 60
+   #Distance_traveled = (Degree / 360) * Circumference 
+   #Speed = Distance_traveled * time_in_min
    # Angular Speed ω = 2πn/Nt
    # ω = angular speed (rad/s)
    # n = number of pulses
    # t = sampling period (s)
    # N = pulses per rotation
    # fasfsg
-   #fasfaf
-   n = read_num_Of_pulses()
-   t = read_time()
-   N = read_pulse_per_rotation
-   ω = 2 * math.pi * n / N * t
-   return Speed 
+   # fasfaf
+   # Linear 
+   # We want linear 
+   #n = read_num_Of_pulses()
+   #t = read_time()
+   #N = read_pulse_per_rotation
+   #ω = 2 * math.pi * n / N * t
+
+    ## vel = angular * radius 
+    # Use 360 
+    # 
+
+   return calculate_angular_velocity(pulses_per_second) * diameter / 2
 
 ## Publish that number in float format.
+# Change the node name and the topic and message type
 def Publishers():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+    pub = rospy.Publisher('sensor_msgs_encoder', float, queue_size = 10)
+    rospy.init_node('encoder_raw_data_node', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        speed = "The Speed: %f" % pub.calculateSpeed
-        pub.publish(speed)
+        period = 1 # seconds
+        velocity = calculate_linear_velocity(get_num_Of_pulses(period)) # meter per seconds
+        pub.publish(velocity)
         rate.sleep()
    
     if __name__ == '__main__':
