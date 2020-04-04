84f1d275631095e05b934232570f304fbacd0cb1
Madhuri Vempati <sirimadhuri.v@gmail.com>
2020-04-04 17:30:59 -0400

    updated scripts 2

--- commu-intell/ros/src/gps_raw_data/scripts/get_gps_server.py
+++ commu-intell/ros/src/gps_raw_data/scripts/get_gps_server.py
@@ -2,7 +2,7 @@ import rospy
 from gps_raw_data.srv import gps_srv, gps_srvResponse
 
 def handle_reuest():
-    return gps_srvResponse(latitude, longitude)
+    return gps_srvResponse(100, 100)
 
 def get_gps_server():
     rospy.init_node('get_gps_server')
