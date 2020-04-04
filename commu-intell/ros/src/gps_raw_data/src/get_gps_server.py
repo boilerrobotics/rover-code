e64abc3a002803a838ebffe9893959fde52019ae
Madhuri Vempati <sirimadhuri.v@gmail.com>
2020-04-04 18:27:50 -0400

    update server and source code

--- commu-intell/ros/src/gps_raw_data/src/get_gps_server.py
+++ commu-intell/ros/src/gps_raw_data/src/get_gps_server.py
@@ -3,7 +3,10 @@ from gps_raw_data.srv import gps_srv, gps_srvResponse
 from gps_raw_data import readLatLng
 
 def handle_requests():
-    coordinates = readLatLng()
+    port = raw_input('Enter port: ')
+    baudrate = raw_input('Enter baud rate: ')
+    timeout = raw_input('Enter timout: ')
+    coordinates = readLatLng(port, baudrate, timeout)
     return gps_srvResponse(coordinates[0], coordinates[1])
 
 def get_gps_server():
--- commu-intell/ros/src/gps_raw_data/src/gps_raw_data.py
+++ commu-intell/ros/src/gps_raw_data/src/gps_raw_data.py
@@ -3,12 +3,9 @@ import time
 import string
 import pynmea2
 
-def readLatLng():
-	coordinates = []
-	port = "/dev/ttyAMA0" #this port depends on the port you connect the gps to the Pi
-	#you may have to change it accordingly
-	ser = serial.Serial(port, baudrate = 9600, timeout = 0.5) #open serial port
-	#reads every 0.5 seconds
+def readLatLng(port, baudrate, timeout):
+	coordinates = [] #list to hold latitude and longitude
+	ser = serial.Serial(port, baudrate, timeout) #gets data at timeout rate
 	dataout = pynmea2.NMEAStreamReader()
 	newdata = ser.readline()
 
