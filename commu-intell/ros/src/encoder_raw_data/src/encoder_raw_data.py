14bbe3224bf01b55d5e8a8335f33e6858203eb48
pkhai@purdue.edu <pkhai@purdue.edu>
2020-04-04 19:38:20 -0400

    changes with document

--- commu-intell/ros/src/encoder_raw_data/src/encoder_raw_data.py
+++ commu-intell/ros/src/encoder_raw_data/src/encoder_raw_data.py
@@ -1,16 +1,30 @@
-## Find a way to import some signal from the encoder
 import math as math
 import rospy
 from std_msgs.msg._String import String
+#==========================================================================================#
+# This program calculate and publish the linear velocity of the encoder                    #
+#                                                                                          #
+# Written by: Pum Khai                                                                     #
+# Start Date: April 3, 2020                                                                #
+# Last Edit: April 4, 2020                                                                 #   
+#                                                                                          #
+# Status:Incomplete (Need working on get_num_of_pulese(period) function)                   #
+# and getEncoder once get the hardware                                                     #
+#                                                                                          #    
+# Maintance:                                                                               #                            
+#==========================================================================================#
 
 # keeping counting pulses
 # def getEncoder once get the hardware
 
 def get_num_Of_pulses(period):
     # return number of pulses in that period
+    # KEEP WORKING ON IT
    return 10
 
-## Some how find a way to Calculate rotation per seconds
+
+# DO NOT CHANGE ANYTHING BELOW
+## Some how find a way to Calculate linear_velocity
 def calculate_angular_velocity(pulses_per_second):
     pulses_per_rotations = 30 # go to encoder data sheet
     return (pulses_per_second / pulses_per_rotations * 2 * math.pi)
@@ -20,7 +34,6 @@ def calculate_linear_velocity(pulses_per_second):
    return calculate_angular_velocity(pulses_per_second) * diameter / 2
 
 ## Publish that number in float format.
-# Change the node name and the topic and message type
 def Publishers():
     pub = rospy.Publisher('sensor_msgs_encoder', float, queue_size = 10)
     rospy.init_node('encoder_raw_data_node', anonymous=True)
