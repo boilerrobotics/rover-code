fddef6233650d948cbb220e8240ddfa5c9094519
Madhuri Vempati <sirimadhuri.v@gmail.com>
2020-04-14 19:31:35 -0400

    updated

--- commu-intell/ros/setup.bash
+++ commu-intell/ros/setup.bash
@@ -5,3 +5,5 @@ chmod +x src/beginner_tutorials/srv/*
 chmod +x src/beginner_tutorials/scripts/*
 chmod +x src/imu/src/scripts/*
 chmod +x src/motor/src/scripts/*
+chmod +x src/gps_pose/src/*
+chmod +x src/gps_pose/srv/*
