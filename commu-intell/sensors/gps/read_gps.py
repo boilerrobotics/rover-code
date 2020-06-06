1a2180f57ed953db1ae44c5622ba68dfc85eaca4
tame0001 <thirawat.tam@gmail.com>
2020-06-06 20:12:41 +0000

    calculate distance

--- commu-intell/sensors/gps/read_gps.py
+++ commu-intell/sensors/gps/read_gps.py
@@ -1,8 +1,20 @@
 import pynmea2
 import serial
-
+import math
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5.0)
+
+earth_radius = 6371000 # meters
+
+target_location = {
+    'latitude': 13.214337,
+    'longitude': 100.938345
+}
+
+prev_location = {
+    'latitude': 0.0,
+    'longitude': 0.0
+}
 
 while True:
     try:
@@ -14,6 +26,27 @@ while True:
             print('Longitude(raw): {}'.format(msg.longitude))
             print('Speed (from GPS): {:.3f} m/s'.format(msg.spd_over_grnd*0.514444))
             print('Timestamp: {} {} UTC'.format(msg.datestamp, msg.timestamp))
+            print('Current Position: {:.6f}, {:.6f}'.format(msg.latitude, msg.longitude))
+
+            dif_long = math.radians(msg.longitude - target_location['longitude'])
+            dif_lat = math.radians(msg.latitude - target_location['latitude'])
+
+            lat1_radian = math.radians(msg.latitude)
+            lat2_radian = math.radians(target_location['latitude'])
+
+            a = math.sin(dif_lat/2) * math.sin(dif_lat/2) \
+                + math.sin(dif_long/2) * math.sin(dif_long/2) \
+                * math.cos(lat1_radian) * math.cos(lat2_radian)
+
+            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+            distance = c * earth_radius
+
+            print('Target Position: {:.6f}, {:.6f}'.format(target_location['latitude'], target_location['longitude']))
+            print('Distance from target (meters): {:,}'.format(int(distance)))
+
+            prev_location['latitude'] = msg.latitude
+            prev_location['longitude'] = msg.longitude
+            
             
     except serial.SerialException as e:
         print('Device error: {}'.format(e))
