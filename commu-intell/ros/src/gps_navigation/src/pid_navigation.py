e193433fe54e12661689046ec7aaca11c4d5b646
tame0001 <thirawat.tam@gmail.com>
2020-06-18 23:44:48 +0000

    intregrate calculate heading function with ros

---
+++ commu-intell/ros/src/gps_navigation/src/pid_navigation.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+"""
+===============================================================================
+Program Description 
+	This program will process gps raw data and genetare command to turning robot.
+
+Author:         Thirawat Bureetes(Thirawat.tam@gmail.com)
+Maintainer:     Thirawat Bureetes(Thirawat.tam@gmail.com)
+Version:        June 18, 2020
+Status:         In progress
+===============================================================================
+"""
+
+import rospy
+from gps_pose.msg import GpsPose
+
+import math
+import datetime
+import pytz
+from dateutil.parser import parse
+
+
+class GpsNavigation:
+
+    RADIAS = 6371000  # meters
+    # DTFORMAT = ''
+
+    def __init__(self, dest_lat, dest_long):
+        self.destination = GpsPose()
+        self.destination.latitude = dest_lat
+        self.destination.longitude = dest_long
+
+        self.prev_coordinate = None
+
+        self.gps_data = GpsPose()
+        rospy.init_node('gps_pid_navigation', anonymous=True)
+        rospy.Subscriber('gps_raw', GpsPose, self.gps_raw_callback)
+        rospy.spin()
+
+    def gps_raw_callback(self, gps):
+
+        self.gps_data.latitude = float(gps.latitude)
+        self.gps_data.longitude = float(gps.longitude)
+        self.gps_data.raw_speed = float(gps.raw_speed)
+        self.gps_data.last_update = parse(gps.last_update).astimezone(
+            pytz.timezone("America/Indianapolis"))
+
+        print('Latitude: {}'.format(self.gps_data.latitude))
+        print('Longitude: {}'.format(self.gps_data.longitude))
+        print('Last Update: {}'.format(self.gps_data.last_update.isoformat()))
+        print('Ground Speed: {}'.format(self.gps_data.raw_speed))
+        print('***************************************')
+
+        if self.prev_coordinate is None:
+            self.prev_coordinate = self.gps_data
+            return
+
+        distance = self.calulate_distance(self.gps_data, self.destination)
+
+        print('Target Position: {:.6f}, {:.6f}'.format(
+            self.destination.latitude, self.destination.longitude))
+        print('Distance from target meter(s): {:,}'.format(int(distance)))
+
+        target_direction = self.calulate_heading(self.gps_data, self.destination)
+        print('Direction to target: {:.2f} degrees CCW'.format(
+            target_direction))   
+        
+        current_direction = self.calulate_heading(self.prev_coordinate, self.gps_data)
+        print('Current Direction: {:.2f} degrees CCW'.format(
+            current_direction))   
+
+        print('---------------------------------------')
+
+        self.prev_coordinate = self.gps_data
+
+    def calulate_distance(self, loc_a, loc_b):
+        dif_long = math.radians(loc_b.longitude - loc_a.longitude)
+        dif_lat = math.radians(loc_b.latitude - loc_a.latitude)
+
+        lat1_radian = math.radians(loc_a.latitude)
+        lat2_radian = math.radians(loc_b.latitude)
+        
+        a = math.sin(dif_lat/2) * math.sin(dif_lat/2) +  \
+            math.sin(dif_long/2) * math.sin(dif_long/2) * \
+            math.cos(lat1_radian) * math.cos(lat2_radian)
+
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+        distance = c * self.RADIAS
+
+        return distance
+
+    def calulate_heading(self, loc_a, loc_b):
+        dif_long = math.radians(loc_b.longitude - loc_a.longitude)
+        dif_lat = math.radians(loc_b.latitude - loc_a.latitude)
+
+        heading = math.degrees(math.atan2(dif_lat, dif_long))
+
+        if heading < 0:
+            heading += 360
+
+        return heading
+
+if __name__ == '__main__':
+    destination_latitute = 30.46422
+    destination_longitude = -86.9477585
+    navigation = GpsNavigation(destination_latitute, destination_longitude)
--- commu-intell/ros/src/gps_pose/src/gps_raw_data_sub.py
+++ commu-intell/ros/src/gps_pose/src/gps_raw_data_sub.py
@@ -3,7 +3,7 @@
 """
 ===============================================================================
 Program Description 
-	This program will publish raw GPS data once the data is sent from the sensor.
+	This program will subcribe raw GPS data once the data is sent from the sensor.
 
 Author:         Thirawat Bureetes(Thirawat.tam@gmail.com)
 Maintainer:     Thirawat Bureetes(Thirawat.tam@gmail.com)
@@ -21,7 +21,7 @@ def callback(gps):
     gps_data.longitude = gps.longitude
     gps_data.last_update = gps.last_update
     gps_data.raw_speed = gps.raw_speed
-    
+
     print('Latitude: {}'.format(gps_data.latitude))
     print('Longitude: {}'.format(gps_data.longitude))
     print('Last Update: {}'.format(gps_data.last_update))
