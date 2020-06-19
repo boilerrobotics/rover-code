931377c58eab9769860c9c73c44ef18a03724230
Daniel Lawson <daniellawson9999@gmail.com>
2020-06-19 18:29:14 -0400

    ready to start testing transformer

--- robot-arm/temporary/input_transformer.py
+++ robot-arm/temporary/input_transformer.py
@@ -20,35 +20,72 @@ class Controller:
             self.bindings = json.load(binding_json)
 
     def start(self):
+        # function for parsing binding strings
+        # ex input: 'buttons[11]', output: (buttons, 11)
+        def parse_binding(binding):
+            bracket_index = binding.index('[')
+            action_type = binding[0:bracket_index]
+            number = binding[bracket_index: bracket_index + 1]
+            return action_type, number
+
         def joy_callback(data):
             # do stuff w/ data
 
             # determine the form of input being used (joystick vs controller
             # and then publish the actions
 
+            # create message
             actions_msg = Actions()
-            actions_msg.joints = []
+            # name of joint at index in actions_msg.joints
+            joint_names = ["lift", "shoulder", "elbow", "grip"]
+            # array containing joint update values 
+            actions_msg.joints = [0] * len(joint_names)
+
+
+            # position is for inverse, currently not being used
             actions_msg.position = []
             actions_msg.forwards = True
+
+            # default speed
+            actions_msg.speed = 1
             
+            input = ''
             # the joystick has 6 axes (and 11 buttons)
             if ((len(data.axes) == 6) and (len(data.buttons11 == 11)):
-                joystick = True
+                input = 'joystick'
             else:
                 # controlller code here, change else to elif,
                 # and else to handle an unkown controller
-                joystick = False
+                input = 'controller'
             
             # bind actions by iterating through the bindings dictionary
-            for key in bindings:
-                pass # todo
-
-
-
-
-
+            for key in self.bindings:
+                # if the key is a joint
+                if key in joint_names
+                    binding = self.bindings[key][input]
+                    action_type, number = parse_binding(binding)
+                    actions_msg.joints[joint_names.index(key)] = data[action_type][number]
+                elif key == 'speed':
+                    # determine the keys for 2x and .5x speeds
+                    speed_double_input = self.bindings.speed['2'][input]
+                    speed_half_input = self.bindings.speed['.5'][input]
+                    double_type, double_number = parse_binding(speed_double_input)
+                    half_type, half_number = parse_binding(speed_half_input)
+                    # boolean values for whether half/double speeds are presssed
+                    double = data[double_type][double_number]
+                    half = data[half_type, half_number]
+                    # if both pressed, stay at 1x so check that just one pressed
+                    if double and not half:
+                        actions_msg.speed = 2
+                    else if half and not double:
+                        actions_msg.speed = .5
+                    else:
+                        actions_msg.speed = 1
+            
+            # publish the fully formed msg
             self.publisher.publish(actions_msg)
         
+        # subscribe to joy, the source of raw controller inptu
         rospy.Subscriber("joy", Joy, joy_callback)
 
         # spin() keeps python from exiting until node is stopped
