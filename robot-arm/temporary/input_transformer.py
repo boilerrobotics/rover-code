b20676ade106606ca9c1801ca11b786b5b51827a
Daniel Lawson <daniellawson9999@gmail.com>
2020-06-19 17:20:06 -0400

    workign on input_transformations

--- robot-arm/temporary/input_transformer.py
+++ robot-arm/temporary/input_transformer.py
@@ -15,14 +15,38 @@ class Controller:
         # create node
         rospy.init_node('input_transformer', anonymous=True)
 
+        # load the binding json
+        with open('bindings.json') as binding_json:
+            self.bindings = json.load(binding_json)
+
     def start(self):
         def joy_callback(data):
             # do stuff w/ data
+
+            # determine the form of input being used (joystick vs controller
             # and then publish the actions
+
             actions_msg = Actions()
             actions_msg.joints = []
             actions_msg.position = []
             actions_msg.forwards = True
+            
+            # the joystick has 6 axes (and 11 buttons)
+            if ((len(data.axes) == 6) and (len(data.buttons11 == 11)):
+                joystick = True
+            else:
+                # controlller code here, change else to elif,
+                # and else to handle an unkown controller
+                joystick = False
+            
+            # bind actions by iterating through the bindings dictionary
+            for key in bindings:
+                pass # todo
+
+
+
+
+
             self.publisher.publish(actions_msg)
         
         rospy.Subscriber("joy", Joy, joy_callback)
