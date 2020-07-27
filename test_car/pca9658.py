c3863064e8409aec24b6f45fcbdd97e8d0902b01
tame0001 <thirawat.tam@gmail.com>
2020-07-27 01:56:31 +0000

    test pwm generator with python

---
+++ test_car/pca9658.py
@@ -0,0 +1,26 @@
+from board import SCL, SDA
+import busio
+import time
+
+from adafruit_pca9685 import PCA9685
+from adafruit_motor import servo
+
+i2c_bus = busio.I2C(SCL, SDA)
+pca = PCA9685(i2c_bus)
+pca.frequency = 60
+
+# steering = servo.Servo(pca.channels[0], min_pulse=220, max_pulse=440)
+steering = servo.Servo(pca.channels[0])
+
+for i in range(40, 140, 5):
+    steering.angle = i
+    print(i)
+    time.sleep(1)
+for i in range(40, 140, 5):
+    steering.angle = 180 - i
+    print(i)
+    time.sleep(1)
+
+steering.angle = 90
+
+pca.deinit()
