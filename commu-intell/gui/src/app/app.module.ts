dbfd63e1deeec49ad2d878b980d8d52d4bb681d9
tame0001 <thirawat.tam@gmail.com>
2020-10-28 15:00:15 -0400

    template for gps information

--- commu-intell/gui/src/app/app.module.ts
+++ commu-intell/gui/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MiniRoverComponent } from './mini-rover/mini-rover.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiniRoverComponent
   ],
   imports: [
     BrowserModule,
---
+++ commu-intell/gui/src/app/mini-rover/mini-rover.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-rover',
+  templateUrl: './mini-rover.component.html',
+  styleUrls: ['./mini-rover.component.css']
+})
+export class MiniRoverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
