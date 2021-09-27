47314ee57b2b81f496b98fbc97ae53113d1c0fc2
tame0001 <thirawat.tam@gmail.com>
2021-09-27 17:51:38 -0400

    modify demo talker

--- commu-intell/ros2/src/tutorial/setup.py
+++ commu-intell/ros2/src/tutorial/setup.py
@@ -4,7 +4,7 @@ package_name = 'tutorial'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -14,13 +14,13 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='tam',
-    maintainer_email='tam@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer_email='thirawat.tam@gmail.com',
+    description='Tutorial for learning how to use ROS2',
+    license='MIT License',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = tutorial.publisher_member_function:main',
+            'talker = tutorial.publisher:main',
         ],
     },
 )
---
+++ commu-intell/ros2/src/tutorial/tutorial/publisher.py
@@ -0,0 +1,45 @@
+'''
+===============================================================================
+Program Description 
+	This program is a demo publisher.
+
+Author:         Thirawat Bureetes(Thirawat.tam@gmail.com)
+Maintainer:     Thirawat Bureetes(Thirawat.tam@gmail.com)
+Update:         June 16, 2020
+Version:        0.1.0
+===============================================================================
+'''
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from datetime import datetime
+
+
+class Publisher(Node):
+
+    def __init__(self):
+        super().__init__('demo_publisher')
+        self.publisher_ = self.create_publisher(String, 'demo', 10)
+        interval = 1  # seconds
+        self.timer = self.create_timer(interval, self.timer_callback)
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = f'Current time is {datetime.now().replace(microsecond=0).isoformat()}'
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: {msg.data}')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    publisher = Publisher()
+
+    rclpy.spin(publisher)
+    publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
