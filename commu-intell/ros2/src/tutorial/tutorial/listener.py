e26ce086ec606a7f495589caf90a362cd340013f
tame0001 <thirawat.tam@gmail.com>
2021-11-05 16:12:48 -0400

    update pub sub with custom msg

--- commu-intell/ros2/src/tutorial/tutorial/listener.py
+++ commu-intell/ros2/src/tutorial/tutorial/listener.py
@@ -5,20 +5,21 @@ Program Description
 
 Author:         Thirawat Bureetes(Thirawat.tam@gmail.com)
 Maintainer:     Thirawat Bureetes(Thirawat.tam@gmail.com)
-Update:         September 29, 2021
-Version:        0.1.0
+Update:         November 05, 2021
+Version:        0.2.0
 ===============================================================================
 '''
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from tutorial_interfaces.msg import Timestamp
+from datetime import date, datetime
 class Subscriber(Node):
 
     def __init__(self):
         super().__init__('demo_subscriber')
         self.subscription = self.create_subscription(
-            String,
+            Timestamp,
             'demo',
             self.listener_callback,
             10)
@@ -26,7 +27,15 @@ class Subscriber(Node):
 
     def listener_callback(self, msg):
         # self.get_logger().info('I heard: "%s"' % msg.data)
-        self.get_logger().info(f'Recieved: {msg.data}')
+        timestamp = datetime(
+            year = msg.year,
+            month = msg.month,
+            day = msg.day,
+            hour = msg.hour,
+            minute = msg.minute,
+            second = msg.second,
+        )
+        self.get_logger().info(f'Recieved: {timestamp.isoformat()}')
 
 
 def main(args=None):
--- commu-intell/ros2/src/tutorial/tutorial/publisher.py
+++ commu-intell/ros2/src/tutorial/tutorial/publisher.py
@@ -5,28 +5,36 @@ Program Description
 
 Author:         Thirawat Bureetes(Thirawat.tam@gmail.com)
 Maintainer:     Thirawat Bureetes(Thirawat.tam@gmail.com)
-Update:         September 27, 2021
-Version:        0.1.0
+Update:         November 05, 2021
+Version:        0.2.0
 ===============================================================================
 '''
 
 import rclpy
+import pytz
 from rclpy.node import Node
-from std_msgs.msg import String
+from tutorial_interfaces.msg import Timestamp
 from datetime import datetime
 class Publisher(Node):
 
     def __init__(self):
         super().__init__('demo_publisher')
-        self.publisher_ = self.create_publisher(String, 'demo', 10)
+        self.publisher_ = self.create_publisher(Timestamp, 'demo', 10)
         interval = 1  # seconds
         self.timer = self.create_timer(interval, self.timer_callback)
 
     def timer_callback(self):
-        msg = String()
-        msg.data = f'Current time is {datetime.now().replace(microsecond=0).isoformat()}'
+        msg = Timestamp()
+        current_time = datetime.now(pytz.utc).replace(microsecond=0)
+        msg.year = current_time.year
+        msg.month = current_time.month
+        msg.day = current_time.day
+        msg.hour = current_time.hour
+        msg.minute = current_time.minute
+        msg.second = current_time.second
+        msg.timezone = int(current_time.utcoffset().total_seconds()/(60**2))
         self.publisher_.publish(msg)
-        self.get_logger().info(f'Publishing: {msg.data}')
+        self.get_logger().info(f'Publishing: {msg}')
 
 
 def main(args=None):
