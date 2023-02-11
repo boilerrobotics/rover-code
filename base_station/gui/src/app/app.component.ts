e7d04978fb51d137641dafb3950334e876a5355c
tame0001 <thirawat.tam@gmail.com>
2023-02-11 13:27:33 -0500

    Setup mqtt client

--- base_station/gui/src/app/app.component.ts
+++ base_station/gui/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IMqttMessage, MqttService } from 'ngx-mqtt';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy{
   title = 'Boiler Robotics';
+  private subscription!: Subscription;
+  public message!: string;
+
+  constructor(private _mqttService: MqttService) { 
+    this.subscription = this._mqttService.observe('brc')
+    .subscribe((message: IMqttMessage) => {
+      this.message = message.payload.toString();
+      console.log(this.message);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 }
--- base_station/gui/src/app/app.module.ts
+++ base_station/gui/src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MqttModule, IMqttServiceOptions } from 'ngx-mqtt';
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: 'broker.mqttdashboard.com',
+  port: 8000,
+  protocol: 'ws',
+  path: '/mqtt'
+}
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS)
   ],
   providers: [],
   bootstrap: [AppComponent]
