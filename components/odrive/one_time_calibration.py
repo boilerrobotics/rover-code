f291cfe1f1f6c71b7576ee557579643861c6d295
Mordred2 <owenlbishop@gmail.com>
2023-02-21 20:08:16 -0500

    Many Motors

--- components/odrive/one_time_calibration.py
+++ components/odrive/one_time_calibration.py
@@ -3,25 +3,37 @@ from odrive.enums import *
 import time
 
 # Find a connected ODrive (this will block until you connect one)
-print("finding an odrive...")
-my_drive = odrive.find_any()
+print("finding odrives...")
+right_serial = "206737A14152"
+left_serial = "208E31834E53"
+front_serial = ""
 
-print(my_drive.serial_number)
-print(my_drive.vbus_voltage)
+right_drive = odrive.find_any(right_serial)
+left_drive = odrive.find_any(left_serial)
+front_drive = odrive.find_any(front_serial)
+axes = [right_drive.axis0, right_drive.axis1, left_drive.axis0, left_drive.axis1, front_drive.axis0, front_drive.axis1]
+drives = [right_drive, left_drive, front_drive]
 
-print(f'Axis error code = {my_drive.axis1.error}')
-print(f'Encoder error code = {my_drive.axis1.encoder.error}')
+for drive in drives:
+    print(drive.serial_number)
+    print(drive.vbus_voltage)
 
-my_drive.axis1.encoder.config.use_index = True
-my_drive.axis1.requested_state = AxisState.ENCODER_INDEX_SEARCH
-while AxisState(my_drive.axis1.current_state) != AxisState.IDLE:
-    time.sleep(0.1)
+for axis in axes:
+    print(f"Calibrating {axis}")
+    print(f'Axis error code = {axis.error}')
+    print(f'Encoder error code = {axis.encoder.error}')
 
-my_drive.axis1.requested_state = AxisState.ENCODER_OFFSET_CALIBRATION
-while AxisState(my_drive.axis1.current_state) != AxisState.IDLE:
-    time.sleep(0.1)
+    axis.encoder.config.use_index = True
+    axis.requested_state = AxisState.ENCODER_INDEX_SEARCH
+    while AxisState(axis.axis1.current_state) != AxisState.IDLE:
+        time.sleep(0.1)
 
-my_drive.axis1.encoder.config.pre_calibrated = True
-my_drive.axis1.motor.config.pre_calibrated = True
+    axis.requested_state = AxisState.ENCODER_OFFSET_CALIBRATION
+    while AxisState(axis.current_state) != AxisState.IDLE:
+        time.sleep(0.1)
 
-my_drive.save_configuration()
+    axis.encoder.config.pre_calibrated = True
+    axis.motor.config.pre_calibrated = True
+
+for drive in drives:
+    drive.save_configuration()
--- components/odrive/test_run.py
+++ components/odrive/test_run.py
@@ -3,31 +3,29 @@ from odrive.enums import *
 import time
 
 # Find a connected ODrive (this will block until you connect one)
-print("finding an odrive...")
-left = odrive.find_any(serial_number="208E31834E53")
-print("finding another odrive...")
-right = odrive.find_any(serial_number="206737A14152")
+print("finding odrives...")
+right_serial = "206737A14152"
+left_serial = "208E31834E53"
+front_serial = ""
 
 #Right: 206737A14152
 #35627687035218
 #Left: 208E31834E53
 #35795088133715
 
+right_drive = odrive.find_any(right_serial)
+left_drive = odrive.find_any(left_serial)
+front_drive = odrive.find_any(front_serial)
+axes = [right_drive.axis0, right_drive.axis1, left_drive.axis0, left_drive.axis1, front_drive.axis0, front_drive.axis1]
+drives = [right_drive, left_drive, front_drive]
+
+for axis in axes:
 # print(f'Axis error code = {my_drive.axis0.error}')
 # print(f'Encoder error code = {my_drive.axis0.encoder.error}')
+    axis.requested_state = AxisState.CLOSED_LOOP_CONTROL
 
-left.axis0.requested_state = AxisState.CLOSED_LOOP_CONTROL
-right.axis0.requested_state = AxisState.CLOSED_LOOP_CONTROL
-left.axis1.requested_state = AxisState.CLOSED_LOOP_CONTROL
-right.axis1.requested_state = AxisState.CLOSED_LOOP_CONTROL
-
-
-left.axis0.controller.input_vel = 2
-right.axis0.controller.input_vel = 2
-left.axis1.controller.input_vel = 2
-right.axis1.controller.input_vel = 2
+for axis in axes:
+    axis.controller.input_vel = 2
 time.sleep(3)
-left.axis0.controller.input_vel = 0
-right.axis0.controller.input_vel = 0
-left.axis1.controller.input_vel = 0
-right.axis1.controller.input_vel = 0
+for axis in axes:
+    axis.controller.input_vel = 0
