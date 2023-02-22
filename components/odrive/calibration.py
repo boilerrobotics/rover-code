90f51222e05a4b2973316187be1bdc87d655af50
rafifi7 <79894190+rafifi7@users.noreply.github.com>
2023-02-21 20:06:32 -0500

    Update calibration.py

--- components/odrive/calibration.py
+++ components/odrive/calibration.py
@@ -3,45 +3,44 @@ from odrive.enums import *
 import time
 
 # Find a connected ODrive (this will block until you connect one)
-print("finding an odrive...")
-my_drive = odrive.find_any()
 
-print(my_drive.serial_number)
-print(my_drive.vbus_voltage)
-
-print(f'Axis error code = {my_drive.axis1.error}')
-print(f'Encoder error code = {my_drive.axis1.encoder.error}')
-with open('pre-cal.txt', 'w') as f:
-    f.write(f'Axis: \n')
-    f.write(str(my_drive.axis1.config))
-    f.write('\n\n---------------------------\n\n')
-    f.write('\nMotor: \n')
-    f.write(str(my_drive.axis1.motor.config))
-    f.write('\n\n---------------------------\n\n')
-    f.write('\nEncoder: \n')
-    f.write(str(my_drive.axis1.encoder.config))
-
+print("finding odrives...")
+right_serial = "206737A14152"
+left_serial = "208E31834E53"
+front_serial = ""
+right_drive = odrive.find_any(right_serial)
+left_drive = odrive.find_any(left_serial)
+front_drive = odrive.find_any(front_serial)
+axes = [right_drive.axis0, right_drive.axis1, left_drive.axis0, left_drive.axis1, front_drive.axis0, front_drive.axis1]
+drives = [right_drive, left_drive, front_drive]
 
 # Calibrate motor and wait for it to finish
 print("starting calibration...")
-my_drive.axis1.requested_state = AxisState.FULL_CALIBRATION_SEQUENCE
-while AxisState(my_drive.axis1.current_state) != AxisState.IDLE:
-    time.sleep(0.1)
+for axis in axes:
+    print(f'Axis error code = {axis.error}')
+    print(f'Encoder error code = {axis.encoder.error}')
+    axis.requested_state = AxisState.FULL_CALIBRATION_SEQUENCE
+    axis.motor.config.pole_pairs = 7
+    axis.motor.config.calibration_current = 20.0
+    axis.motor.config.motor_type = 0
+    axis.motor.config.resistance_calib_max_voltage = 4.0
+    axis.motor.config.requested_current_range = 20.0
+    axis.motor.config.current_control_bandwidth = 100.0
+    axis.motor.config.torque_constant = 8.27/270
+    axis.controller.config.control_mode = 2
+    axis.controller.config.vel_limit = 10
+    axis.encoder.config.mode = 1
+    axis.encoder.config.cpr = 42
+    axis.encoder.config.pre_calibrated = True
+    axis.encoder.config.bandwidth = 100
+    while AxisState(axis.current_state) != AxisState.IDLE:
+        time.sleep(0.1)
 
-print(f'Axis error code = {my_drive.axis1.error}')
-print(f'Encoder error code = {my_drive.axis1.encoder.error}')
-with open('post-cal.txt', 'w') as f:
-    f.write(f'Axis: \n')
-    f.write(str(my_drive.axis1.config))
-    f.write('\n\n---------------------------\n\n')
-    f.write('\nMotor: \n')
-    f.write(str(my_drive.axis1.motor.config))
-    f.write('\n\n---------------------------\n\n')
-    f.write('\nEncoder: \n')
-    f.write(str(my_drive.axis1.encoder.config))
+    print(f'Axis error code = {axis.error}')
+    print(f'Encoder error code = {axis.encoder.error}')
 
-my_drive.axis1.requested_state = AxisState.CLOSED_LOOP_CONTROL
+    axis.requested_state = AxisState.CLOSED_LOOP_CONTROL
 
-my_drive.axis1.controller.input_vel = 2
-time.sleep(3)
-my_drive.axis1.controller.input_vel = 0
+    axis.controller.input_vel = 2
+    time.sleep(3)
+    axis.controller.input_vel = 0
