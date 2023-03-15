6bdd0dad7b5ab132b36f4e6e1ed0719ab735113d
tame0001 <thirawat.tam@gmail.com>
2023-03-14 20:55:18 -0400

    revise odrive test run

--- components/odrive/test_run.py
+++ components/odrive/test_run.py
@@ -1,35 +1,52 @@
+'''
+This code is for testing odrive functionality.
+** Need to impliment using async **
+'''
+
+import yaml
 import odrive
 from odrive.enums import *
 import time
 
-# Find a connected ODrive (this will block until you connect one)
+with open('config.yml') as fp:
+    config = yaml.safe_load(fp) 
+
 print("finding odrives...")
-right_serial = "206737A14152"
-left_serial = "208E31834E53"
-front_serial = "2071316B4E53"
-
-#Right: 206737A14152
-#35627687035218
-#Left: 208E31834E53
-#35795088133715
-
-# right_drive = odrive.find_any(serial_number = right_serial)
-# left_drive = odrive.find_any(serial_number = left_serial)
-front_drive = odrive.find_any(serial_number = front_serial)
-axes = [front_drive.axis0, front_drive.axis1] #[right_drive.axis0, right_drive.axis1, left_drive.axis0, left_drive.axis1]#, front_drive.axis0, front_drive.axis1]
-drives = [front_drive] #[right_drive, left_drive]#, front_drive]
-
-for drive in drives:
-    print(drive.serial_number)
-    print(drive.vbus_voltage)
-    print()
-
-for axis in axes:
-# print(f'Axis error code = {my_drive.axis0.error}')
-# print(f'Encoder error code = {my_drive.axis0.encoder.error}')
-    axis.requested_state = AxisState.CLOSED_LOOP_CONTROL
-    axis.controller.input_vel = 2
-    print("test")
-time.sleep(3)
-for axis in axes:
-    axis.controller.input_vel = 0
+odrvs = {}
+for section, serial in config['serial'].items():
+    print(f'searching for serial number {serial}...')
+    try: 
+        odrv = odrive.find_any(serial_number=serial, timeout=1)
+        odrvs[section] = odrv
+        print(f'-> assign odrive {serial} to {section} section')
+    except TimeoutError as e:
+        print(f'error: Cannot find serial {serial} !!')
+print('--------------------------------------')
+for section, odrv in odrvs.items(): # Checking errors
+    print(f'{section} odrive checking...') 
+    print(f'  voltage = {odrv.vbus_voltage:5.2f} V')
+    print(f'  current = {odrv.ibus:5.2f} A')
+    print(f'  {"error code:":<13}axis0{" "*10}axis1')
+    print(f'  {"controller":<10}{odrv.axis0.controller.error:6}'
+          f'{odrv.axis1.controller.error:15}')
+    print(f'  {"encoder":<10}{odrv.axis0.encoder.error:6}'
+          f'{odrv.axis1.encoder.error:15}')
+    print(f'  {"motor":<10}{odrv.axis0.motor.error:6}'
+          f'{odrv.axis1.motor.error:15}')
+    print('--------------------------------------')
+    test_run_duration = 3
+    test_run_speed = 5
+for section, odrv in odrvs.items(): # Test run
+    print(f'Test run {section}...')    
+    odrv.axis0.requested_state = AxisState.CLOSED_LOOP_CONTROL
+    odrv.axis1.requested_state = AxisState.CLOSED_LOOP_CONTROL
+    odrv.axis0.controller.input_vel = test_run_speed
+    odrv.axis1.controller.input_vel = test_run_speed
+    for _ in range(test_run_duration):
+        print(f'  voltage = {odrv.vbus_voltage:5.2f} V')
+        print(f'  current = {odrv.ibus:5.2f} A')
+        time.sleep(1)
+    odrv.axis0.controller.input_vel = 0
+    odrv.axis1.controller.input_vel = 0
+    print(f'  voltage = {odrv.vbus_voltage:5.2f} V')
+    print(f'  current = {odrv.ibus:5.2f} A')
