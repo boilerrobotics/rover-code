45c84520645dd7bf2059b7af8612f7e267af4e67
tame0001 <thirawat.tam@gmail.com>
2023-03-18 12:26:54 -0400

    move check error and find odrive to util

--- components/odrive/test_run.py
+++ components/odrive/test_run.py
@@ -3,13 +3,12 @@ This code is for testing odrive functionality.
 ** Need to impliment using async **
 '''
 import time
-import yaml
+
 import utils
-import odrive
 from odrive.enums import *
 
-def run_seconds(odrv, running_time: int, 
-                running_speed: int = 3, delay_time: int = 2) -> None:
+def run_seconds(odrv, running_time: int, running_speed: int = 3, 
+                delay_time: int = 3, debug_interval: int = 1) -> None:
     '''
     Run odrive for running_time seconds. 
     Delay for delay seconds both before and after running.
@@ -19,63 +18,39 @@ def run_seconds(odrv, running_time: int,
     odrv.axis1.requested_state = AxisState.CLOSED_LOOP_CONTROL
     for _ in range(delay_time): 
         utils.print_voltage_current(odrv)
-        time.sleep(1)
+        time.sleep(debug_interval)
     odrv.axis0.controller.input_vel = running_speed
     odrv.axis1.controller.input_vel = running_speed
     for _ in range(running_time): 
         utils.print_voltage_current(odrv)
-        time.sleep(1)
+        time.sleep(debug_interval)
     odrv.axis0.controller.input_vel = 0
     odrv.axis1.controller.input_vel = 0
     for _ in range(delay_time): 
         utils.print_voltage_current(odrv)
-        time.sleep(1)
+        time.sleep(debug_interval)
     odrv.axis0.controller.input_vel = -running_speed
     odrv.axis1.controller.input_vel = -running_speed
     for _ in range(running_time): 
         utils.print_voltage_current(odrv)
-        time.sleep(1)
+        time.sleep(debug_interval)
     odrv.axis0.controller.input_vel = 0
     odrv.axis1.controller.input_vel = 0
     for _ in range(delay_time): 
         utils.print_voltage_current(odrv)
-        time.sleep(1)
+        time.sleep(debug_interval)
 
 def test_run(odrvs, running_time=5, running_speed=3) -> None:
     '''
     Take a dictionary of section:odrive and perform test run.
     '''
-    for section, odrv in odrvs.items(): # Checking errors
-        print(f'{section} odrive checking...') 
-        utils.print_voltage_current(odrv)
-        print(f'  {"error code:":<13}axis0{" "*10}axis1')
-        # How can we get error code from enum
-        print(f'  {"controller":<10}{odrv.axis0.controller.error:6}'
-            f'{odrv.axis1.controller.error:15}')
-        print(f'  {"encoder":<10}{odrv.axis0.encoder.error:6}'
-            f'{odrv.axis1.encoder.error:15}')
-        print(f'  {"motor":<10}{odrv.axis0.motor.error:6}'
-            f'{odrv.axis1.motor.error:15}')
-        print('--------------------------------------')
-    for section, odrv in odrvs.items(): # Test run
+    for section, odrv in odrvs.items(): 
+        utils.check_error(odrv, section) # Checking errors
         print(f'Test run {section}...')    
-        run_seconds(odrv, running_time, running_speed)
-'''
+        run_seconds(odrv, running_time, running_speed) # Test run
+''' 
 -------------------------------------------------------------------------------
 '''
 if __name__ == '__main__':
-    with open('config.yml') as fp:
-        config = yaml.safe_load(fp) 
-
-    print("finding odrives...")
-    odrvs = {} # Looking for avaiable ODrive
-    for section, serial in config['serial'].items():
-        print(f'searching for serial number {serial}...')
-        try: 
-            odrv = odrive.find_any(serial_number=serial, timeout=1)
-            odrvs[section] = odrv
-            print(f'-> assign odrive {serial} to {section} section')
-        except TimeoutError as e:
-            print(f'error: Cannot find serial {serial} !!')
-    print('--------------------------------------')
+    odrvs = utils.find_odrvs()
     test_run(odrvs, running_time=5, running_speed=3) # Call test run fucntion
--- components/odrive/utils.py
+++ components/odrive/utils.py
@@ -2,9 +2,44 @@
 Utility functions for ODrive calibration and test.
 '''
 
+import yaml
+import odrive
+
 def print_voltage_current(odrv) -> None:
     '''
     Print voltage and current for debugging.
     '''
     print(f'  voltage = {odrv.vbus_voltage:5.2f} V'
           f'  current = {odrv.ibus:5.2f} A')
+    
+def find_odrvs() -> dict:
+    with open('config.yml') as fp:
+        config = yaml.safe_load(fp) 
+
+    print("finding odrives...")
+    odrvs = {} # Looking for avaiable ODrive
+    for section, serial in config['serial'].items():
+        print(f'searching for serial number {serial}...')
+        try: 
+            odrv = odrive.find_any(serial_number=serial, timeout=1)
+            odrvs[section] = odrv
+            print(f'-> assign odrive {serial} to {section} section')
+        except TimeoutError as e:
+            print(f'error: Cannot find serial {serial} !!')
+    print('--------------------------------------')
+
+    return odrvs
+
+def check_error(odrv, name: str | None = None) -> None:
+    if name is not None: 
+        print(f'{name} odrive checking...') 
+    print_voltage_current(odrv)
+    print(f'  {"error code:":<13}axis0{" "*10}axis1')
+    # How can we get error code from enum
+    print(f'  {"controller":<10}{odrv.axis0.controller.error:6}'
+        f'{odrv.axis1.controller.error:15}')
+    print(f'  {"encoder":<10}{odrv.axis0.encoder.error:6}'
+        f'{odrv.axis1.encoder.error:15}')
+    print(f'  {"motor":<10}{odrv.axis0.motor.error:6}'
+        f'{odrv.axis1.motor.error:15}')
+    print('--------------------------------------')
