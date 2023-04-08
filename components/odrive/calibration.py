3e34969a79dd677a153be97cc1c4e0d32a94cc17
Mordred2 <owenlbishop@gmail.com>
2023-04-08 12:04:25 -0400

    One Script Calibration

--- components/odrive/calibration.py
+++ components/odrive/calibration.py
@@ -25,14 +25,14 @@ def dump_config(odrv, filename) -> None:
 
 def config_controller(controller) -> None:
     controller.config.control_mode = ControlMode.VELOCITY_CONTROL
-    controller.config.pos_gain = 1
+    #controller.config.pos_gain = 1
     controller.config.vel_limit = 10
 
 def config_motor(motor) -> None:
     motor.config.pole_pairs = 7
     motor.config.calibration_current = 20.0
     motor.config.motor_type = MotorType.HIGH_CURRENT
-    motor.config.resistance_calib_max_voltage = 4.0
+    motor.config.resistance_calib_max_voltage = 5.0
     motor.config.requested_current_range = 20.0
     motor.config.current_control_bandwidth = 100.0
     motor.config.torque_constant = 8.27/270
@@ -45,24 +45,26 @@ def config_encoder(encoder) -> None:
     encoder.config.bandwidth = 100
     encoder.config.calib_scan_distance = 150
 
-odrvs = utils.find_odrvs()
-for section, odrv in odrvs.items(): 
-    utils.check_error(odrv, section) # Checking errors
-    dump_config(odrv, filename=f'{section}-precal.txt')
-    print(f'Calicating {section}...')    
-    odrv.config.brake_resistance = 0.5
-    for axis in [odrv.axis0, odrv.axis1]:
-        config_controller(axis.controller)
-        config_motor(axis.motor)
-        config_encoder(axis.encoder)
-        axis.requested_state = AxisState.FULL_CALIBRATION_SEQUENCE
-        while axis.current_state != AxisState.IDLE:
-            utils.print_voltage_current(odrv)
-            time.sleep(1)
-        utils.check_error(odrv, section) # Check error again
-    odrv.save_configuration() 
-    dump_config(odrv, filename=f'{section}-postcal.txt')
-    print(f'Section {section} calibration completed')
-print('Test run ...')
-test_run.test_run(odrvs, running_time=5, running_speed=3)
+def run_calib(odrvs):
+    for section, odrv in odrvs.items(): 
+        utils.check_error(odrv, section) # Checking errors
+        dump_config(odrv, filename=f'{section}-precal.txt')
+        print(f'Calibrating {section}...')    
+        odrv.config.brake_resistance = 0.5
+        for axis in [odrv.axis0, odrv.axis1]:
+            config_controller(axis.controller)
+            config_motor(axis.motor)
+            config_encoder(axis.encoder)
+            axis.requested_state = AxisState.FULL_CALIBRATION_SEQUENCE
+            while axis.current_state != AxisState.IDLE:
+                utils.print_voltage_current(odrv)
+                time.sleep(1)
+            utils.check_error(odrv, section) # Check error again
+        odrv.save_configuration() 
+        dump_config(odrv, filename=f'{section}-postcal.txt')
+        print(f'Section {section} calibration completed')
+
+if __name__ == '__main__':
+    odrvs = utils.find_odrvs()
+    run_calib(odrvs)
 
--- components/odrive/one_time_calibration.py
+++ components/odrive/one_time_calibration.py
@@ -4,23 +4,27 @@ from odrive.enums import *
 import time
 
 # Find a connected ODrive (this will block until you connect one)
-odrvs = utils.find_odrvs()
 
-for section, odrv in odrvs.items(): 
-    utils.check_error(odrv, section)
-    print(f'Calibating {section}...')    
-
-    for axis in [odrv.axis0, odrv.axis1]:
-        axis.encoder.config.use_index = True
-        axis.requested_state = AxisState.ENCODER_INDEX_SEARCH
-        while axis.current_state != AxisState.IDLE:
-            utils.print_voltage_current(odrv)
-            time.sleep(1)
-        axis.requested_state = AxisState.ENCODER_OFFSET_CALIBRATION
-        while axis.current_state != AxisState.IDLE:
-            utils.print_voltage_current(odrv)
-            time.sleep(1)
-        axis.encoder.config.pre_calibrated = True
-        axis.motor.config.pre_calibrated = True
+def one_time(odrvs):
+    for section, odrv in odrvs.items(): 
         utils.check_error(odrv, section)
-    odrv.save_configuration()
+        print(f'Calibating {section}...')    
+
+        for axis in [odrv.axis0, odrv.axis1]:
+            axis.encoder.config.use_index = True
+            axis.requested_state = AxisState.ENCODER_INDEX_SEARCH
+            while axis.current_state != AxisState.IDLE:
+                utils.print_voltage_current(odrv)
+                time.sleep(1)
+            axis.requested_state = AxisState.ENCODER_OFFSET_CALIBRATION
+            while axis.current_state != AxisState.IDLE:
+                utils.print_voltage_current(odrv)
+                time.sleep(1)
+            axis.encoder.config.pre_calibrated = True
+            axis.motor.config.pre_calibrated = True
+            utils.check_error(odrv, section)
+        odrv.save_configuration()
+
+if __name__ == '__main__':
+    odrvs = utils.find_odrvs()
+    one_time(odrvs)
